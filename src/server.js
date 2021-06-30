408124da7916a8d1a8d36199055287bba252efc2
LucasSantosDev <lucas_ss.arts@live.com>
2021-06-29 22:53:52 -0300

    change location of procfile

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./index');
 const { PORT_API_LISTEN } = require('./constants');
 
-app.listen(PORT_API_LISTEN, function () {
+app.listen(process.env.PORT || PORT_API_LISTEN, function () {
   console.log(`LISTEN >>> ${PORT_API_LISTEN}`)
 });
